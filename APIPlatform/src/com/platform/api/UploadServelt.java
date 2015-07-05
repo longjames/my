b4cdbc4e133719e42bbeb5b03282fdbb8afc2a19
longjames <824499468@qq.com>
2015-07-05 15:12:56 +0800

    0.1
    修改了一些SQL语句和字段，外加了一些包

---
+++ APIPlatform/src/com/platform/api/UploadServelt.java
@@ -0,0 +1,10 @@
+package com.platform.api;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/upload")
+public class UploadServelt extends HttpServlet {
+//	private String getFilename
+	//	public void doPost()
+
+}
